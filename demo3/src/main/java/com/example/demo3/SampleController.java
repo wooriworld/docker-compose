53a9504202334b757f4abfef5bf14308e112ece1
cheadev <cheadev@naver.com>
2022-03-11 23:40:33 +0900

    Initial commit

---
+++ demo3/src/main/java/com/example/demo3/SampleController.java
@@ -0,0 +1,12 @@
+package com.example.demo3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+    @GetMapping("/ex1")
+    public void ex1() {
+
+    }
+}
---
+++ demo3/src/test/java/com/example/demo3/Demo3ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo3;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat("1").isEqualTo("1");
+    }
+
+}
